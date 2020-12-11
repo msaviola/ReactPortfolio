e822db61356b0dcd4f7d72c0cdb4a344551984da
msaviola <67287151+msaviola@users.noreply.github.com>
2020-12-10 21:28:36 -0800

    bones

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+{/* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"></link> */}
